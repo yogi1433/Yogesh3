1d8d0795ad7e3866ebb54b2608fd8387fab5bd84
yogi1433 <parnapalliyogeshhanuman@gmail.com>
2023-02-07 20:23:49 +0530

    message

---
+++ Yogesh96.java
@@ -0,0 +1,13 @@
+class Yogesh96
+{
+static void replace1()
+{
+String str="Ramu";
+{System.out.println(str);
+System.out.println(str.replace('u',' '));
+}}
+public static void main(String[] args)
+{
+replace1();
+}
+}
